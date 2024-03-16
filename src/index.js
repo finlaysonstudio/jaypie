e21cc88921b4f908e4452a343e12525722a5819d
Adam Finlayson <amf@adamfinlayson.com>
2024-03-16 13:25:34 -0500

    docs: add comments

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 // Export
 //
 
+// Required dependencies
 export * from "@jaypie/core";
 
+// Optional dependencies are wrapped in a dynamic import
 export * from "./aws.package.js";
 export * from "./lambda.package.js";
 export * from "./mongoose.package.js";
