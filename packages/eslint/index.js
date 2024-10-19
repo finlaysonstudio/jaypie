7e9929e4b29d130f14644c1ea407faab318c534c
Adam Finlayson <amf@adamfinlayson.com>
2024-10-19 12:23:42 -0500

    chore: remove: cruft

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -9,8 +9,6 @@ export default [
   // Configs and Plugins
   //
   js.configs.recommended, // Recommended config applied to all files
-  // TODO: plugins import pluginNoAutofix
-  // ...pluginVue.configs["flat/essential"],
   pluginPrettierRecommended, // Prettier wants to always be the last plugin
 
   //
