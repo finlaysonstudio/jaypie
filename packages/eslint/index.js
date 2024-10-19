0fbe05a170920f8f648a57813598b050abb459f6
Adam Finlayson <amf@adamfinlayson.com>
2024-10-18 21:54:33 -0500

    chore: install and config prettier

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,6 +1,18 @@
 import js from "@eslint/js";
+import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
+  //
+  //
   // Configs and Plugins
+  //
   js.configs.recommended, // Recommended config applied to all files
+  // TODO: plugins import pluginNoAutofix/
+  // ...pluginVue.configs["flat/essential"],
+  pluginPrettierRecommended, // Prettier wants to always be the last plugin
+  
+  //
+  //
+  // Project Overrides
+  //
 ];
