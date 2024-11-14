465274d45084e0868906a36a3d9090555ff38696
Adam Finlayson <amf@adamfinlayson.com>
2024-11-14 09:59:49 -0600

    feat: export  error from status code function

--- packages/core/src/lib/errors/index.js
+++ packages/core/src/lib/errors/index.js
@@ -13,3 +13,4 @@ export default exportErrors;
 
 export * from "./errors.js";
 export { default as isJaypieError } from "./isJaypieError.function.js";
+export { errorFromStatusCode } from "./errorFromStatusCode.js";
