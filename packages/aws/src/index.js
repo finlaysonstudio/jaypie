930d463e6a28e4dd1697b240a26d987926b8dfee
Adam Finlayson <amf@adamfinlayson.com>
2024-11-25 10:24:54 -0600

    chore: config: export get singleton message

--- packages/aws/src/index.js
+++ packages/aws/src/index.js
@@ -1,4 +1,5 @@
 export { default as getMessages } from "./getMessages.function.js";
 export { default as getSecret } from "./getSecret.function.js";
+export { default as getSingletonMessage } from "./getSingletonMessage.function.js";
 export { default as sendBatchMessages } from "./sendBatchMessages.function.js";
 export { default as sendMessage } from "./sendMessage.function.js";
