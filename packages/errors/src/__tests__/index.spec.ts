4de61d53f78a935e1820e4d7d1e3d0ea15937198
Adam Finlayson <amf@adamfinlayson.com>
2024-12-23 21:46:37 -0600

    test: error exports

--- packages/errors/src/__tests__/index.spec.ts
+++ packages/errors/src/__tests__/index.spec.ts
@@ -16,4 +16,20 @@ describe("errors/index", () => {
       expect(typeof BadRequestError).toBe("function");
     });
   });
+
+  describe("Happy Paths", () => {
+    it("creates a BadRequestError", () => {
+      const error = new BadRequestError("test");
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe("test");
+      expect(error.status).toBe(400);
+      expect(error.title).toBe("Bad Request");
+      try {
+        throw error;
+      } catch (e) {
+        expect(e).toBe(error);
+      }
+      expect.assertions(5);
+    });
+  });
 });
