89c9ed7ad64dfd585ee8565a8eb55a209dabf197
Adam Finlayson <amf@adamfinlayson.com>
2024-12-27 20:08:19 -0600

    chore: config: rollup

--- packages/testkit/rollup.config.js
+++ packages/testkit/rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 
 export default {
+  external: ["@jaypie/core", "jest-json-schema", "lodash.isequal", "vitest"],
   input: "src/index.ts",
   output: {
     dir: "dist",
