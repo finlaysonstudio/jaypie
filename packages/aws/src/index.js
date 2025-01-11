a9dd2345db2e66609a0082ed2ba10a014de75964
Adam Finlayson <amf@adamfinlayson.com>
2025-01-11 15:57:23 -0600

    chore: config: export send textract job

--- packages/aws/src/index.js
+++ packages/aws/src/index.js
@@ -3,3 +3,4 @@ export { default as getSecret } from "./getSecret.function.js";
 export { default as getSingletonMessage } from "./getSingletonMessage.function.js";
 export { default as sendBatchMessages } from "./sendBatchMessages.function.js";
 export { default as sendMessage } from "./sendMessage.function.js";
+export { default as sendTextractJob } from "./sendTextractJob.function.js";
