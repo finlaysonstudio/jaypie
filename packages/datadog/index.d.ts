13636ec1ccefdba3c54a1b16ad7588c42eda8201
Adam Finlayson <amf@adamfinlayson.com>
2025-02-03 17:07:41 -0600

    chore: config: types for datadog

---
+++ packages/datadog/index.d.ts
@@ -0,0 +1,43 @@
+export interface DatadogEnv {
+  DATADOG_API_KEY: string;
+  DD_SITE: string;
+  SECRET_DATADOG_API_KEY: string;
+}
+
+export interface DatadogMetricType {
+  UNKNOWN: 0;
+  COUNT: 1;
+  RATE: 2;
+  GAUGE: 3;
+}
+
+export interface DatadogConstants {
+  ENV: DatadogEnv;
+  METRIC: {
+    TYPE: DatadogMetricType;
+  };
+  SITE: string;
+}
+
+export const DATADOG: DatadogConstants;
+
+export interface SubmitMetricOptions {
+  apiKey?: string;
+  apiSecret?: string;
+  name: string;
+  type?: DatadogMetricType[keyof DatadogMetricType];
+  value: number;
+  tags?: Record<string, string>;
+  timestamp?: number;
+}
+
+export function submitMetric(options: SubmitMetricOptions): Promise<boolean>;
+
+export interface SubmitMetricSetOptions
+  extends Omit<SubmitMetricOptions, "value"> {
+  values: number[];
+}
+
+export function submitMetricSet(
+  options: SubmitMetricSetOptions,
+): Promise<boolean>;
