d0fa295727106abe5d0abd818ed10b35c2831b58
Adam Finlayson <amf@adamfinlayson.com>
2025-02-13 23:45:57 -0600

    chore: convert to typescript

--- packages/testkit/src/placeholders.ts
+++ packages/testkit/src/placeholders.ts
@@ -13,18 +13,22 @@
  * @param  {*}            def  A default value to return [optional]
  * @return {*}                 The value
  */
-function get(obj, path, def) {
+function get(
+  obj: Record<string, unknown>,
+  path: string | string[],
+  def?: unknown,
+): unknown {
   /**
    * If the path is a string, convert it to an array
    * @param  {String|Array} path The path
    * @return {Array}             The path array
    */
-  const stringToPath = (path) => {
+  const stringToPath = (path: string | string[]): string[] => {
     // If the path isn't a string, return it
-    if (typeof path !== "string") return path;
+    if (typeof path !== "string") return path as string[];
 
     // Create new array
-    const output = [];
+    const output: string[] = [];
 
     // Split to an array with dot notation
     path.split(".").forEach((item) => {
@@ -44,15 +48,20 @@ function get(obj, path, def) {
   path = stringToPath(path);
 
   // Cache the current object
-  let current = obj;
+  let current: unknown = obj;
 
   // For each item in the path, dig into the object
   for (let i = 0; i < path.length; i++) {
     // If the item isn't found, return the default (or null)
-    if (!current[path[i]]) return def;
+    if (
+      !current ||
+      typeof current !== "object" ||
+      !(path[i] in (current as object))
+    )
+      return def;
 
-    // Otherwise, update the current  value
-    current = current[path[i]];
+    // Otherwise, update the current value
+    current = (current as Record<string, unknown>)[path[i]];
   }
 
   return current;
@@ -70,7 +79,10 @@ function get(obj, path, def) {
  * @param {String} template The template string
  * @param {String} local    A local placeholder to use, if any
  */
-function placeholders(template, data) {
+function placeholders(
+  template: string | (() => string),
+  data?: Record<string, unknown>,
+): string {
   // Check if the template is a string or a function
   template = typeof template === "function" ? template() : template;
   if (["string", "number"].indexOf(typeof template) === -1)
@@ -80,16 +92,16 @@ function placeholders(template, data) {
   if (!data) return template;
 
   // Replace our curly braces with data
-  template = template.replace(/\{\{([^}]+)\}\}/g, (match) => {
+  template = template.replace(/\{\{([^}]+)\}\}/g, (match: string) => {
     // Remove the wrapping curly braces
     match = match.slice(2, -2);
 
     // Get the value
-    const val = get(data, match.trim());
+    const val = get(data, match.trim(), undefined);
 
     // Replace
     if (!val) return `{{${match}}}`;
-    return val;
+    return String(val);
   });
 
   return template;
