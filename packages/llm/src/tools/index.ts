530c9d4826d21e8cee4021e29f3bfd76a8df763c
Adam Finlayson <amf@adamfinlayson.com>
2025-03-20 21:58:12 -0500

    feat: init: weather tool

--- packages/llm/src/tools/index.ts
+++ packages/llm/src/tools/index.ts
@@ -1,11 +1,13 @@
 import { random } from "./random.js";
 import { roll } from "./roll.js";
 import { time } from "./time.js";
+import { weather } from "./weather.js";
 
 export const toolkit = {
   random,
   roll,
   time,
+  weather,
 };
 
 export const tools = Object.values(toolkit);
