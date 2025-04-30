ccaf0b559d9b552d9789fa8379dcf7a0ca440f83
Adam Finlayson <amf@adamfinlayson.com>
2025-04-29 22:13:44 -0500

    fix: test: mock

--- packages/textract/vitest.setup.js
+++ packages/textract/vitest.setup.js
@@ -5,4 +5,22 @@ import { expect, vi } from "vitest";
 expect.extend(extendedMatchers);
 expect.extend(jaypieMatchers);
 
-vi.mock("jaypie", async () => vi.importActual("@jaypie/testkit/mock"));
+vi.mock("jaypie", async () => {
+  return {
+    ...(await vi.importActual("@jaypie/testkit/mock")),
+    log: {
+      debug: vi.fn(),
+      error: vi.fn(),
+      fatal: vi.fn(),
+      info: vi.fn(),
+      init: vi.fn(),
+      lib: vi.fn(),
+      tag: vi.fn(),
+      trace: vi.fn(),
+      untag: vi.fn(),
+      var: vi.fn(),
+      warn: vi.fn(),
+      with: vi.fn(),
+    }
+  };
+});
