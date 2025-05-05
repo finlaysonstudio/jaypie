3936f18fad7238fb62fbf1a61f577d2449b656e6
Adam Finlayson <amf@adamfinlayson.com>
2025-05-04 22:06:25 -0500

    chore: config: rollup

--- packages/testkit/rollup.config.js
+++ packages/testkit/rollup.config.js
@@ -4,7 +4,13 @@ import copy from "rollup-plugin-copy";
 
 export default [
   {
-    external: ["@jaypie/core", "jest-extended", "jest-json-schema", "vitest"],
+    external: [
+      "@jaypie/core",
+      "jest-extended",
+      "jest-json-schema",
+      "vitest",
+      "node:util",
+    ],
     input: "src/index.ts",
     output: {
       dir: "dist",
