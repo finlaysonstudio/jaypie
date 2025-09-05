eae9fc92c812e4b8abdaf538e52a8865ace3e374
Adam Finlayson <amf@adamfinlayson.com>
2025-09-04 21:33:27 -0500

    feat: has datadog env function

--- packages/datadog/src/index.js
+++ packages/datadog/src/index.js
@@ -4,6 +4,7 @@
 //
 
 export { DATADOG } from "./constants.js";
+export { default as hasDatadogEnv } from "./hasDatadogEnv.function.js";
 export { default as submitDistribution } from "./submitDistribution.adapter.js";
 export { default as submitMetric } from "./submitMetric.adapter.js";
 export { default as submitMetricSet } from "./submitMetricSet.adapter.js";
