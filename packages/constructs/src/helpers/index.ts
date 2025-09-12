3b5104539bb9859d956055ef308d456fa8686e90
Adam Finlayson <amf@adamfinlayson.com>
2025-09-12 18:10:42 -0500

    feat: jaypie lambda env

--- packages/constructs/src/helpers/index.ts
+++ packages/constructs/src/helpers/index.ts
@@ -3,3 +3,4 @@ export { constructStackName } from "./constructStackName";
 export { constructTagger } from "./constructTagger";
 export { envHostname } from "./envHostname";
 export { isEnv, isProductionEnv, isSandboxEnv } from "./isEnv";
+export { jaypieLambdaEnv } from "./jaypieLambdaEnv";
