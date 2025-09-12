22afbb63216665e7a750ce0ff807588fcd8b7852
Adam Finlayson <amf@adamfinlayson.com>
2025-09-12 14:14:01 -0500

    chore: config: rename stack to construct tagger

--- packages/constructs/src/helpers/constructTagger.ts
+++ packages/constructs/src/helpers/constructTagger.ts
@@ -1,4 +1,5 @@
-import { Stack, Tags } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Tags } from "aws-cdk-lib";
 import { constructStackName } from "./constructStackName";
 
 const CDK = {
@@ -26,8 +27,8 @@ const CDK = {
   },
 };
 
-export function stackTagger(
-  stack: Stack,
+export function constructTagger(
+  construct: Construct,
   { name }: { name?: string } = {},
 ): boolean {
   const stackName = name || constructStackName();
@@ -35,29 +36,29 @@ export function stackTagger(
     process.env.npm_package_version || process.env.PROJECT_VERSION || null;
 
   if (process.env.PROJECT_COMMIT && process.env.PROJECT_COMMIT.length > 8) {
-    Tags.of(stack).add(
+    Tags.of(construct).add(
       CDK.TAG.BUILD_HEX,
       process.env.PROJECT_COMMIT.slice(0, 8),
     );
   }
-  Tags.of(stack).add(CDK.TAG.BUILD_DATE, new Date().toISOString());
-  Tags.of(stack).add(CDK.TAG.BUILD_TIME, Date.now().toString());
+  Tags.of(construct).add(CDK.TAG.BUILD_DATE, new Date().toISOString());
+  Tags.of(construct).add(CDK.TAG.BUILD_TIME, Date.now().toString());
   if (process.env.PROJECT_COMMIT)
-    Tags.of(stack).add(CDK.TAG.COMMIT, process.env.PROJECT_COMMIT);
-  Tags.of(stack).add(CDK.TAG.CREATION, CDK.CREATION.CDK);
+    Tags.of(construct).add(CDK.TAG.COMMIT, process.env.PROJECT_COMMIT);
+  Tags.of(construct).add(CDK.TAG.CREATION, CDK.CREATION.CDK);
   if (process.env.PROJECT_ENV)
-    Tags.of(stack).add(CDK.TAG.ENV, process.env.PROJECT_ENV);
+    Tags.of(construct).add(CDK.TAG.ENV, process.env.PROJECT_ENV);
   if (process.env.PROJECT_NONCE)
-    Tags.of(stack).add(CDK.TAG.NONCE, process.env.PROJECT_NONCE);
+    Tags.of(construct).add(CDK.TAG.NONCE, process.env.PROJECT_NONCE);
   if (process.env.PROJECT_KEY)
-    Tags.of(stack).add(CDK.TAG.PROJECT, process.env.PROJECT_KEY);
-  Tags.of(stack).add(CDK.TAG.ROLE, CDK.ROLE.STACK);
+    Tags.of(construct).add(CDK.TAG.PROJECT, process.env.PROJECT_KEY);
+  Tags.of(construct).add(CDK.TAG.ROLE, CDK.ROLE.STACK);
   if (process.env.PROJECT_SERVICE)
-    Tags.of(stack).add(CDK.TAG.SERVICE, process.env.PROJECT_SERVICE);
+    Tags.of(construct).add(CDK.TAG.SERVICE, process.env.PROJECT_SERVICE);
   if (process.env.PROJECT_SPONSOR)
-    Tags.of(stack).add(CDK.TAG.SPONSOR, process.env.PROJECT_SPONSOR);
-  if (stackName) Tags.of(stack).add(CDK.TAG.STACK, stackName);
-  if (version) Tags.of(stack).add(CDK.TAG.VERSION, version);
+    Tags.of(construct).add(CDK.TAG.SPONSOR, process.env.PROJECT_SPONSOR);
+  if (stackName) Tags.of(construct).add(CDK.TAG.STACK, stackName);
+  if (version) Tags.of(construct).add(CDK.TAG.VERSION, version);
 
   return true;
 }
--- packages/constructs/src/helpers/index.ts
+++ packages/constructs/src/helpers/index.ts
@@ -1,4 +1,4 @@
 export { constructEnvName } from "./constructEnvName";
 export { constructStackName } from "./constructStackName";
 export { isEnv, isProductionEnv, isSandboxEnv } from "./isEnv";
-export { stackTagger } from "./stackTagger";
+export { constructTagger } from "./constructTagger";
