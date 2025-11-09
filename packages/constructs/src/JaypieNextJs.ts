fb363413d3bbb4ec1c818a85d9e455331f8da3b0
Adam Finlayson <amf@adamfinlayson.com>
2025-11-08 19:38:05 -0600

    chore: config: connect nextjs to secrets

--- packages/constructs/src/JaypieNextJs.ts
+++ packages/constructs/src/JaypieNextJs.ts
@@ -4,17 +4,21 @@ import {
   resolveHostedZone,
 } from "@jaypie/constructs";
 import { IHostedZone } from "aws-cdk-lib/aws-route53";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
 import { Nextjs } from "cdk-nextjs-standalone";
 import { Construct } from "constructs";
 import * as path from "path";
 
 import { addDatadogLayers, resolveParamsAndSecrets } from "./helpers";
+import { JaypieEnvSecret } from "./JaypieEnvSecret.js";
 
 export interface JaypieNextjsProps {
-  domainName?: string;
   datadogApiKeyArn?: string;
+  domainName?: string;
+  envSecrets?: { [key: string]: secretsmanager.ISecret };
   hostedZone?: IHostedZone | string;
   nextjsPath?: string;
+  secrets?: JaypieEnvSecret[];
 }
 
 export class JaypieNextJs extends Construct {
@@ -22,10 +26,32 @@ export class JaypieNextJs extends Construct {
     super(scope, id);
 
     const domainName = props?.domainName || envHostname();
+    const envSecrets = props?.envSecrets || {};
     const nextjsPath = props?.nextjsPath?.startsWith("..")
       ? path.join(process.cwd(), props.nextjsPath)
       : props?.nextjsPath || path.join(process.cwd(), "..", "nextjs");
     const paramsAndSecrets = resolveParamsAndSecrets();
+    const secrets = props?.secrets || [];
+
+    // Process secrets environment variables
+    const secretsEnvironment = Object.entries(envSecrets).reduce(
+      (acc, [key, secret]) => ({
+        ...acc,
+        [`SECRET_${key}`]: secret.secretName,
+      }),
+      {},
+    );
+
+    // Process JaypieEnvSecret array
+    const jaypieSecretsEnvironment = secrets.reduce((acc, secret) => {
+      if (secret.envKey) {
+        return {
+          ...acc,
+          [`SECRET_${secret.envKey}`]: secret.secretName,
+        };
+      }
+      return acc;
+    }, {});
 
     const nextjs = new Nextjs(this, "NextJsApp", {
       nextjsPath,
@@ -35,7 +61,11 @@ export class JaypieNextJs extends Construct {
           zone: props?.hostedZone,
         }),
       },
-      environment: jaypieLambdaEnv(),
+      environment: {
+        ...jaypieLambdaEnv(),
+        ...secretsEnvironment,
+        ...jaypieSecretsEnvironment,
+      },
       overrides: {
         nextjsServer: {
           functionProps: {
@@ -52,5 +82,15 @@ export class JaypieNextJs extends Construct {
 
     addDatadogLayers(nextjs.imageOptimizationFunction);
     addDatadogLayers(nextjs.serverFunction.lambdaFunction);
+
+    // Grant secret read permissions
+    Object.values(envSecrets).forEach((secret) => {
+      secret.grantRead(nextjs.serverFunction.lambdaFunction);
+    });
+
+    // Grant read permissions for JaypieEnvSecrets
+    secrets.forEach((secret) => {
+      secret.grantRead(nextjs.serverFunction.lambdaFunction);
+    });
   }
 }
