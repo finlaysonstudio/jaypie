61b7ef9059882d1798dbb516187aa6b3f253da3f
Adam Finlayson <amf@adamfinlayson.com>
2025-12-11 22:06:02 -0600

    chore: config: lint

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -181,8 +181,9 @@ export default [
       ...vitest.configs.recommended.rules,
       "@typescript-eslint/no-explicit-any": "off",
       "vitest/expect-expect": "warn",
-      "vitest/no-focused-tests": ["error", { fixable: false }],
+      "vitest/no-conditional-expect": "off",
       "vitest/no-disabled-tests": "warn",
+      "vitest/no-focused-tests": ["error", { fixable: false }],
     },
   },
 
